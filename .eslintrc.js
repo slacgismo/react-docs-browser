00d583379fb7675b0b845c848f7e6755977fa05b
Thistleman <codes5096@gmail.com>
2022-03-30 11:07:19 -0700

    Removed prettier to allow eslint to function properly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,47 @@
 module.exports = {
-    env: {
-      es6: true,
-      browser: true,
-      es2021: true,
-      node: true,
+  env: {
+    es6: true,
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-      'plugin:react/recommended',
-      'plugin:react-hooks/recommended',
-      'airbnb',
-    ],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    '@typescript-eslint/indent': 'off',
+    'react/prop-types': 0,
+    'func-names': 0,
+    'react/function-component-definition': [2, { namedComponents: 'function-declaration' }],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+        ],
       },
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    plugins: [
-      'react',
-      'prettier',
     ],
-    rules: {
-      'linebreak-style': 0,
-      'react/prop-types': 0,
-      'func-names': 0,
-      'react/function-component-definition': [2, { namedComponents: 'function-declaration' }],
-      'react/jsx-filename-extension': [
-        1,
-        {
-          'extensions': [
-            '.js',
-            '.jsx'
-          ]
-        }
-      ],
-      'camelcase': 1,
-      'max-len': 1,
-      'jsx-a11y/label-has-associated-control': [
-        2,
-        {
-          'assert': 'either'
-        }
-      ]
-    },
-  };
+    camelcase: 1,
+    'max-len': 1,
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      {
+        assert: 'either',
+      },
+    ],
+  },
+};
