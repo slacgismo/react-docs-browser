710e90b944e07efb3bb37603b6bfbe7a91e14930
Thistleman <codes5096@gmail.com>
2022-03-30 11:33:21 -0700

    Updated webpack to generate proper tab title

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'JobSite',
+      title: 'Docs-Browser',
       template: path.resolve(__dirname, './src/index.html'),
     }),
     new CleanWebpackPlugin(),
