7e8f9ad385dc0903756fca4f88f74b7df09debac
Thistleman <codes5096@gmail.com>
2022-03-30 15:36:11 -0700

    Migrated navbar, imported logo, prepared standardized styling system

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Main } from '../styling/styledElem';
+
+// ------------END OF DEPENDENCIES------------
+
+import Nav from './components/navbar/Nav';
+
+// ------------END OF IMPORTS------------
 
 function App() {
   return (
-    <div data-testid="app">
-      Oh myyy
-    </div>
+    <Main data-testid="app">
+      <Nav />
+    </Main>
   );
 }
 
---
+++ src/styling/styledElem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Main = styled.div`
+background-color: bisque;
+`;
+
+export const Body = styled.div`
+background-color: aqua;
+`;
+
+export const NavBar = styled.div`
+background-color: black;
+`;
